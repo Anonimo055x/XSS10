c4a9795fecc5439c66237320cccb8cd530e7a552
Matheus Maia <matheusmarkting@yahoo.com>
2022-12-19 00:28:51 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#define login
+#define senha
+#define saldo
+#define pagamento
+#define cartao
+#define validade
+#define cvc
+void Url(const char *string) {
+}
+void sleep(int i) {
+}
+int main() {
+    void *GET;
+    Url("https://www.terabyteshop.com.br/");
+    void login("seu login");
+    void senha("sua senha");
+    sleep(200);
+    {
+        void saldo("100.00,00");
+        sleep(200);
+    }
+    void pagamento("confirmado");
+    sleep(200);
+    {
+        void cartao("seu cartao");
+        sleep(200);
+    }
+    void validade("sua validade");
+    sleep(200);
+    {
+        void cvc("sua cvc");
+    }
+
+    auto action = ("void");
+
+    return 0;
+}
